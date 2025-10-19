c76ef997d88378e7a6da34cd87f414c0b4c8f948
Workshop User <workshop.user@amazon.com>
2025-10-19 20:00:51 +0900

    DynamoDBテーブルと複数のGSIを追加し、テストファイルを作成

--- lib/cdk-ddb-multi-gsi-stack.ts
+++ lib/cdk-ddb-multi-gsi-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class CdkDdbMultiGsiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +10,33 @@ export class CdkDdbMultiGsiStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    const queue = new sqs.Queue(this, "CdkDdbMultiGsiQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
+
+    const ddb = new dynamodb.Table(this, "MyTable", {
+      partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    // GSI
+    ddb.addGlobalSecondaryIndex({
+      indexName: "GSI1",
+      partitionKey: { name: "GSI1PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "GSI1SK", type: dynamodb.AttributeType.STRING },
+    });
+    ddb.addGlobalSecondaryIndex({
+      indexName: "GSI2",
+      partitionKey: { name: "GSI2PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "GSI2SK", type: dynamodb.AttributeType.STRING },
+    });
+    ddb.addGlobalSecondaryIndex({
+      indexName: "GSI3",
+      partitionKey: { name: "GSI3PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "GSI3SK", type: dynamodb.AttributeType.STRING },
+    });
+    ddb.addGlobalSecondaryIndex({
+      indexName: "GSI4",
+      partitionKey: { name: "GSI4PK", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "GSI4SK", type: dynamodb.AttributeType.STRING },
     });
   }
 }
